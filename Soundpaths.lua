3165fbcf24f9f413d22412149d5bff81794b7c54
Michael Cook <you@example.com>
2021-01-09 20:28:30 -0800

    initial commit

---
+++ Soundpaths.lua
@@ -0,0 +1,29 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDELoud Noises!: R 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "|cFF0070DELoud Noises!: Fuckin Idiot|r", [[Interface\Addons\SharedMedia_LoudNoises\sound\Fuckin_Idiot.ogg]]) 
+LSM:Register("sound", "|cFF0070DELoud Noises!: Hit My Balls|r", [[Interface\Addons\SharedMedia_LoudNoises\sound\Hit_My_Balls.ogg]]) 
+LSM:Register("sound", "|cFF0070DELoud Noises!: Make a Wish|r", [[Interface\Addons\SharedMedia_LoudNoises\sound\Make_A_Wish.ogg]]) 
+LSM:Register("sound", "|cFF0070DELoud Noises!: Recommend not Wiping|r", [[Interface\Addons\SharedMedia_LoudNoises\sound\Recommend_Not_Wiping.ogg]]) 
+LSM:Register("sound", "|cFF0070DELoud Noises!: Step on my Sanity|r", [[Interface\Addons\SharedMedia_LoudNoises\sound\Step_On_My_Sanity.ogg]]) 
+LSM:Register("sound", "|cFF0070DELoud Noises!: Well Well Well|r", [[Interface\Addons\SharedMedia_LoudNoises\sound\Well_Well_Well.ogg]]) 
+LSM:Register("sound", "|cFF0070DELoud Noises!: Who the Fuck are You?|r", [[Interface\Addons\SharedMedia_LoudNoises\sound\Who_The_Fuck_Are_You.ogg]]) 
+LSM:Register("sound", "|cFF0070DELoud Noises!: Inside another Man's Ass!|r", [[Interface\Addons\SharedMedia_LoudNoises\sound\Wodi_Inside.ogg]]) 
+
+-- -----
+--   STATUSBAR
+-- -----
